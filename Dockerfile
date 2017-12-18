bf8fbc9e8d1f0f2185d3d095fd3055b9419ed755
Andrew Burrows <burrowsa@gmail.com>
2017-12-18 00:11:26 +0000

    Add Alexa support
    Includes support for delegation and synonyms

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.6
-RUN apk add --no-cache python3 python3-dev musl-dev gcc
-RUN pip3 install Flask_SocketIO eventlet boto3
+RUN apk add --no-cache python3 python3-dev musl-dev gcc libffi-dev openssl-dev
+RUN pip3 install Flask_SocketIO eventlet boto3 flask_ask
 RUN addgroup xmasbar && \
     adduser -D -G xmasbar xmasbar
 RUN mkdir /data && chown xmasbar:xmasbar /data
@@ -15,3 +15,4 @@ WORKDIR /xmasbar
 ENV FLASK_APP="backend.py"
 ENTRYPOINT ["flask", "run", "-h", "0.0.0.0", "-p", "5000"]
 EXPOSE 5000
+
--- backend.py
+++ backend.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 from flask import Flask, send_file, request, redirect
 from flask_socketio import SocketIO, emit, disconnect
-import sys
+from flask_ask import Ask, statement, session as ask_session, delegate, request as ask_request
+import sys, json
 from aws import bucket, load_data, save_drink, save_orders, save_quantities, remove_drink
 from binascii import a2b_base64
+import random
 
 
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = b"^\x96\xf2\xbeH\xef='T\xf7<\xe8h\xc8\xa8g\x9c\xb1\x86\x84\xf3w\x15W" 
+ask = Ask(app, '/alexa')
+app.config['ASK_VERIFY_REQUESTS'] = False
 socketio = SocketIO(app, async_mode=None) 
+
+
 _drinks, _orders, _quantities = load_data(bucket)
+
+
 JPEG_DATA_URL_PREFIX = "data:image/jpeg;base64,"
 S3_WEB_URL_TEMPLATE = "http://isthebaropen.s3-website-eu-west-1.amazonaws.com/images/{}.jpg"
 
@@ -25,7 +33,7 @@ def before_request():
 @app.route('/')
 def index():
   return send_file('index.html')
-  
+
 
 def emit_data(orders=False, quantities=False, drinks=False, broadcast=False):
   data = dict()
@@ -35,15 +43,21 @@ def emit_data(orders=False, quantities=False, drinks=False, broadcast=False):
     data['quantities'] = _quantities 
   if drinks:
     data['drinks'] = _drinks
-
-  emit('update', data, broadcast=broadcast)
+  
+  if hasattr(request, 'namespace'): 
+    emit('update', data, broadcast=broadcast)
+  else:
+    socketio.emit('update', data, broadcast=broadcast, namespace='/v1')
 
 
 @socketio.on('place_order', namespace='/v1')
 def v1_place_order(message):
   username = message["username"]
   drink_id = message["drinkId"]
-  
+  _place_order_impl(username, drink_id)
+
+
+def _place_order_impl(username, drink_id):
   user_orders = _orders.setdefault(username, set())
   quantity_remaining = _quantities.get(drink_id, 0)
   if drink_id not in user_orders and quantity_remaining > 0:
@@ -137,11 +151,62 @@ def v1_connect():
   emit_data(orders=True, quantities=True, drinks=True)
 
 
-@socketio.on_error('/v1') # handles the '/chat' namespace
+@socketio.on_error('/v1')
 def v1_error_handler(e):
   emit('error', "An error occured on {}".format(request.event["message"]))
   print(type(e), file=sys.stderr)
   print(e, file=sys.stderr)
+  
+
+def resolve_synonym(name, value):
+  try:
+    return ask_request['intent']['slots'][name]['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name']
+  except KeyError:
+    return value
+
+
+def find_drink(drink):
+  for d_id, d in _drinks.items():
+    if d["name"].lower().strip() == drink.lower().strip():
+      return d_id
+  return None
+  
+
+def find_person(person):
+  for u in _orders:
+    if u.lower().strip() == person.lower().strip():
+      return u
+  return None
+
+
+@ask.intent('OrderDrink')
+def alexa_order_drink(person, drink):
+  if ask_session['dialogState'] != 'COMPLETED' or drink is None or person is None:
+    return delegate(speech=None)  # speech should not be needed
+
+  person = resolve_synonym('person', person)  
+  username = find_person(person)
+  if username is None:
+    return statement("Sorry I don't know {}".format(person))
+
+  drink = resolve_synonym('drink', drink)
+  drink_id = find_drink(drink)
+  if drink_id is None:
+    return statement("Sorry, I could not find a drink called {}".format(drink))
+
+  quantity_remaining = _quantities.get(drink_id, 0)
+
+  if quantity_remaining < 1:
+    return statement("Sorry, but we have run out of {}".format(drink))
+
+  _place_order_impl(username, drink_id)
+    
+  return statement(random.choice([
+                                 "I've notified the butler",
+                                 "Okay",
+                                 "Your drink is on its way",
+                                 "I'll let him know",
+                                 ]))
 
 
 if __name__ == '__main__':
